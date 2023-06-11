d6bef41feb700352aa555d3bf9059f698ec1a681
htaylor91 <htaylor91@gmail.com>
2023-06-11 03:41:20 +0200

    feat(route): 💡 add server side api/employees route
    eventually GET, POST, PUT, etc. requests can be made to this route from the client side once using a database

---
+++ app/api/employees/route.ts
@@ -0,0 +1,16 @@
+/**
+ * @description
+ * API route to add and retrieve employees to and from a database
+ */
+
+/*import { NextResponse, NextRequest } from 'next/server';
+
+export async function GET(request: Request) {
+    const res = await request.json()
+    return NextResponse.json({ res })
+}
+
+export async function POST(request: NextRequest) {
+    const res = await request.json()
+    return NextResponse.json({ res })
+}*/
