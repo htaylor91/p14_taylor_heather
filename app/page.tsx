1f80294c9f8834e227d89074303e7cf97b4b4742
htaylor91 <htaylor91@gmail.com>
2023-06-11 02:45:15 +0200

    feat(Home): 🚸 replace extraneous h1 with h2, add link to NPM package for modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-
 export default function Home() {
   return (
-    <main className={`text-brand-light`}>
-      <h1 className={"text-2xl text-center"}>HRnet</h1>
+    <main className={`text-brand-dark dark:text-brand-light text-center`}>
+      <h2 className={"text-2xl text-center mb-4"}>Modal</h2>
+      <a href="https://www.npmjs.com/package/reactjs-dialog-modal" className={"text-center"}>NPM package</a>
     </main>
   )
 }
